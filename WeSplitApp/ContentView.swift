f7626af7034c163892363d5e444162f20d7177be
NuriddinJumaev <nuriddin.jumaev98>
2020-12-18 12:57:50 +0500

    Split App

--- WeSplitApp/ContentView.swift
+++ WeSplitApp/ContentView.swift
@@ -8,9 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 1
+    
+    let tipPercentages = [0,5,10,15,20,25]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text:$checkAmount)
+                        .keyboardType(.decimalPad)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<21) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section(header: Text("How much tip do you want to leave?").textCase(nil)) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0..<tipPercentages.count){
+                            Text("\(tipPercentages[$0])%")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section{
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+                Image("split").resizable().aspectRatio(contentMode: .fill)
+            }
+            .navigationBarTitle("WeSplit")
+        }
+        
     }
 }
 
